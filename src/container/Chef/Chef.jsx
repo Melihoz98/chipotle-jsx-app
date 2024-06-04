b8583fc4e69ee1f84ec6c4f6c6682fd03fbfdbe3
melihoz13 <10487163@ucn.dk>
2024-06-04 19:17:23 +0200

    newer upd

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
 import './Chef.css';
-
+import { images } from '../../constants'
+import { SubHeading } from '../../components';
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+   <div className='app__wrapper_img app__wrapper_img-reverse'>
+    <img src={images.chef} alt='chef' />
+    </div>
+    <div className='app__wrapper_info'>
+<SubHeading title='Chefs word' />
+<h1 className='headtext__cormorant'>
+  hvad vi tror ppå
+</h1>
+<div className='app__chef-content'>
+  <div className='app__chef-content_quote'>
+    <img src={images.quote} alt='quote' />
+    <p className='p__opensans'> vi har det bedste mad</p>
+  </div>
+  <p className='p__opensans'>
+    meget god mad vi har
+  </p>
+</div>
+
+<div className='app__chef-sign'>
+  <p>Cengiz</p>
+  <p className='p__opensans'>Chef and founder</p>
+  <img src={images.sign} alt='sign' />
+</div>
+
+    </div>
   </div>
 );
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 import './SpecialMenu.css';
+import { SubHeading } from '../../components';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialMenu flex__center section__padding' id='menu'> 
+   <div className='app__specialMenu-title'>
+    <SubHeading title= 'Menu til dig' />
+    <h1 className='headtext__cormoroant'> Vores specialiteter</h1>
+
+   </div>
+
+<div className='app__specialMenu-menu'>
+<div ></div>
+</div>
+
   </div>
 );
 
